9e0d1624ab557741a9d789846f9ef5732a715f50
Mina Mekhael <mina.mekhael@ymail.com>
2019-02-26 22:18:45 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,14 +5,10 @@ class App < Sinatra::Base
   post '/reverse' do
     original_string = params["string"]
     reversed_string = original_string.reverse
- 
+   
     erb :reversed
   end
-
-  post '/reverse' do
-    erb :reversed
-  end
-
+  
   get '/friends' do
     # Write your code here!
 
