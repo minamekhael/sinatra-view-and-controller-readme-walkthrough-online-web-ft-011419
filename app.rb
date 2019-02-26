5166a5801ba4364df976f53c27b4e3c9fef9136b
Mina Mekhael <mina.mekhael@ymail.com>
2019-02-26 22:19:16 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,12 +2,12 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-post '/reverse' do
-  original_string = params["string"]
-  @reversed_string = original_string.reverse
- 
-  erb :reversed
-end
+  post '/reverse' do
+    original_string = params["string"]
+    @reversed_string = original_string.reverse
+   
+    erb :reversed
+  end
   
   get '/friends' do
     # Write your code here!
